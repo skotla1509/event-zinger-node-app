1e40d74cad2c52f59603b7377dec0878553bf438
skotla1509 <sindhukotla1509@gmail.com>
2022-12-14 18:36:08 -0500

    data base object models

--- controllers/users/users-dao.js
+++ controllers/users/users-dao.js
@@ -3,7 +3,7 @@ import usersModel from "./users-model.js";
 export const findAllUsers = async () => await usersModel.find();
 export const findUserById = async (uid) => await usersModel.findById(uid);
 export const findUserByName = async (name) => await usersModel.find({userName: name});
-export const findUserByCredentials = async (username, password) => await usersModel.findOne({username, password});
+export const findUserByCredentials = async (userName, password) => await usersModel.findOne({userName, password});
 export const createUser = async (user) => await usersModel.create(user);
 export const deleteUser = async (uid) => await usersModel.deleteOne({_id: uid});
 export const updateUser = async (uid, user) => await usersModel.updateOne({_id: uid}, {$set: user})
